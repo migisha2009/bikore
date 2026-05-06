fbed1ed3e5a45d816e6d7225cbd029fc3c9543cb
johnson m <migishajohnson24gmail.com>
2026-05-06 07:12:37 +0200

    preview fix

--- frontend/app/(app)/_layout.tsx
+++ frontend/app/(app)/_layout.tsx
@@ -1,4 +1,4 @@
-import { useAuth } from '../../../context/AuthContext';
+import { useAuth } from '../../context/AuthContext';
 import { useEffect } from 'react';
 import { View, Text, StyleSheet, Platform } from 'react-native';
 import { Tabs } from 'expo-router';
